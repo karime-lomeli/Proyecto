88f0e17150ca2622b375cc025f9c8763a16df9c6
karime-lomeli <75227467+karime-lomeli@users.noreply.github.com>
2022-06-06 18:38:16 -0500

    Insert, forms

--- Conexion/models/ADProductos.cs
+++ Conexion/models/ADProductos.cs
@@ -15,18 +15,12 @@ namespace Conexion.models
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
         public string Nombre { get; set; }
-        [BsonElement("nombre")]
         public string Descripcion { get; set; }
-        [BsonElement("descripcion")]
         public int idAlmacen { get; set; }
-        [BsonElement("idalmacen")]
         public int requerido { get; set; }
-        [BsonElement("requerido")]
         public int minimo { get; set; }
-        [BsonElement("minimo")]
         public int stock { get; set; }
-        [BsonElement("stock")]
-        public DateTime fecha { get; set; }
+        public string fecha { get; set; }
 
         public void Insertar()
         {
@@ -34,7 +28,7 @@ namespace Conexion.models
             var database = client.GetDatabase("Lili");
 
             var Productos = database.GetCollection<ADProductos>("Productos");
-            var AdProductos = new ADProductos { Nombre = "Pegamento", Descripcion = "Descripcion", idAlmacen = 1, requerido = 10, minimo = 2, stock = 100 };
+            var AdProductos = new ADProductos { Nombre = "Pegamento", Descripcion = "Descripcion", idAlmacen = 1, requerido = 10, minimo = 2, stock = 9, fecha= "5/1/2008 8:30:52 AM" };
             Productos.InsertOne(AdProductos);
         }
     }
