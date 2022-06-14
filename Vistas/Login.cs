e5228f5e12fac0666ce6d0426c0442a1bef61408
karime-lomeli <75227467+karime-lomeli@users.noreply.github.com>
2022-06-14 17:11:47 -0500

    Sockets

--- Vistas/Login.cs
+++ Vistas/Login.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Controlador;
+using System.Net.Sockets;
+using System.Net;
 
 namespace Vistas
 {
@@ -46,8 +48,62 @@ namespace Vistas
             
             else
             {
+                // Notificacion desde server sockets
+                byte[] bytes = new byte[1024];
 
+                try
+                {
+                    IPHostEntry host = Dns.GetHostEntry("localhost");
+                    IPAddress ipAddress = host.AddressList[0];
+                    IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
+
+                    Socket sender2= new Socket(ipAddress.AddressFamily,SocketType.Stream, ProtocolType.Tcp);
+
+                    try
+                    {
+                        // Connect to Remote EndPoint
+                        sender2.Connect(remoteEP);
+
+                        Console.WriteLine("Socket connected to {0}",
+                            sender2.RemoteEndPoint.ToString());
+
+                        // Encode the data string into a byte array.
+                        byte[] msg = Encoding.ASCII.GetBytes("This is a test<EOF>");
+
+                        // Send the data through the socket.
+                        int bytesSent = sender2.Send(msg);
+
+                        // Receive the response from the remote device.
+                        int bytesRec = sender2.Receive(bytes);
+                        Console.WriteLine("Echoed test = {0}",
+                        Encoding.ASCII.GetString(bytes, 0, bytesRec));
+
+                     
+                    }
+                    catch (ArgumentNullException ane)
+                    {
+                        Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                    }
+                    catch (SocketException se)
+                    {
+                        Console.WriteLine("SocketException : {0}", se.ToString());
+                    }
+                    catch (Exception x)
+                    {
+                        Console.WriteLine("Unexpected exception : {0}", x.ToString());
+                    }
+
+                }
+                catch (Exception eb)
+                {
+                    Console.WriteLine(eb.ToString());
+                }
+                
+                // Fin notificacion
                
+
+                
+
                 Form1 Form = new Form1();
                 Form.IdUsuario = Datos.Rows[0][0].ToString();
                 Form.Apellidos = Datos.Rows[0][1].ToString();
